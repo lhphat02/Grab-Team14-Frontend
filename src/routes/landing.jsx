01e21f8905f53025751c34a9b52dcfe17b4dbbce
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-07 23:48:22 +0700

    feat(landing): Initialize landing page layout

--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -1,11 +1,26 @@
-import IndustrySelectionSlide from '../components/templates/landingSlides/IndustrySelectionSlide';
-import RedirectSlide from '../components/templates/landingSlides/redirectSlide';
-
+import Divider from '../components/common/Divider';
+import LandingSlide from '../components/templates/LandingSlide';
 const LandingPage = () => {
   return (
     <div className="">
-      <IndustrySelectionSlide />
-      <RedirectSlide />
+      <LandingSlide>
+        <h1 className="text-4xl font-black text-center text-prim-1">emploi</h1>
+        <Divider text="Choose your industry" />
+      </LandingSlide>
+      <LandingSlide>
+        <div className="items-center justify-center space-y-4 contain-page">
+          <h1 className="text-4xl font-black text-center text-prim-1">
+            emploi
+          </h1>
+          <h2 className="text-2xl font-semibold text-center text-prim-1">
+            Simplicity for your career
+          </h2>
+          <h2 className="text-lg font-semibold text-center">
+            Stop Searching, Start Thriving. Find Your Ideal Job Here.
+          </h2>
+        </div>
+      </LandingSlide>
+      <LandingSlide>LandingSlide 3 content</LandingSlide>
     </div>
   );
 };
