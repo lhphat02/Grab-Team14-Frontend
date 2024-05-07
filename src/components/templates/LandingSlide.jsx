4a5233766be76a87268ba386e2349c3eaa442bbd
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-07 23:35:24 +0700

    feat(landing): Recreate dynamic Slide Page component

---
+++ src/components/templates/LandingSlide.jsx
@@ -0,0 +1,61 @@
+import { useState, useRef } from 'react';
+import BaseButton from '../common/Button/BaseButton';
+
+const LandingSlide = ({ children, onNext, onPrevious }) => {
+  const [isCurrent, setIsCurrent] = useState(true);
+  const slideRef = useRef(null);
+
+  const handleNext = () => {
+    if (onNext) {
+      onNext();
+    } else {
+      const nextSlide = slideRef?.current?.nextElementSibling;
+      console.log(nextSlide);
+      if (nextSlide) {
+        nextSlide.scrollIntoView({ behavior: 'smooth' });
+        setIsCurrent(false);
+      }
+    }
+  };
+
+  const handleBack = () => {
+    if (onPrevious) {
+      onPrevious();
+    } else {
+      const prevSlide = slideRef.current.previousElementSibling;
+      console.log(prevSlide);
+      if (prevSlide) {
+        prevSlide.scrollIntoView({ behavior: 'smooth' });
+        setIsCurrent(false);
+      }
+    }
+  };
+
+  return (
+    <div
+      ref={slideRef}
+      className={`items-center justify-center space-y-4 container-slide ${
+        isCurrent ? 'active-slide' : ''
+      }`}
+    >
+      {children}
+      <div className="flex flex-row mt-20 space-x-4">
+        <BaseButton
+          onClick={handleBack}
+          className="rounded-full hover:space-x-4 hover:shadow-lg"
+          outline={true}
+        >
+          Back
+        </BaseButton>
+        <BaseButton
+          onClick={handleNext}
+          className="rounded-full hover:space-x-4 hover:shadow-lg"
+        >
+          Next
+        </BaseButton>
+      </div>
+    </div>
+  );
+};
+
+export default LandingSlide;
