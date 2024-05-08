4a5e3ac6f63bf66e641e7da00067ca055dd81741
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-08 13:59:29 +0700

    refactor: Add AuthFormLayout component for consistent layout in authentication forms

---
+++ src/components/layouts/AuthFormLayout.jsx
@@ -0,0 +1,22 @@
+const AuthFormLayout = ({ title, onFormSubmit, formDetails, footer }) => {
+  return (
+    <div className="justify-center bg-gray-100 container-page">
+      <div className="flex flex-col w-full max-w-md px-6 py-4 space-y-6 bg-white shadow-md md:px-12 md:py-8 md:space-y-8 rounded-xl">
+        <div className="md:space-y-2">
+          <h1 className="font-black text-center text-prim-1">emploi</h1>
+          <h5 className="font-semibold text-center text-prim-1">{title}</h5>
+        </div>
+
+        <form className="flex flex-col space-y-6" onSubmit={onFormSubmit}>
+          {formDetails}
+        </form>
+
+        <div className="flex flex-col items-center justify-center w-full space-x-2 md:flex-row">
+          {footer}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthFormLayout;
