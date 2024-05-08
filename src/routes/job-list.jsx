4ce0bfadf5f29bb39cbb7c44812dd80fe20e985a
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-09 03:08:32 +0700

    feat: Update JobListPage to include filter options and search functionality

--- src/routes/job-list.jsx
+++ src/routes/job-list.jsx
@@ -1,5 +1,31 @@
+import BaseSelect from '../components/common/BaseSelect';
+import SearchBar from '../components/common/SearchBar';
+import JobListContainer from '../components/features/JobListPage/JobListContainer';
+import JobPageLayout from '../components/layouts/JobPageLayout';
+import {
+  mockJobData,
+  mockJobArrangementOptions,
+  mockJobIndustryOptions,
+  mockJobLevelOptions,
+  mockJobTypeOptions,
+} from '../constants/mockData';
+
 const JobListPage = () => {
-  return <div>JobListPage</div>;
+  return (
+    <JobPageLayout>
+      <div className="flex justify-between w-full">
+        <div className="flex w-full gap-4">
+          <BaseSelect options={mockJobTypeOptions} />
+          <BaseSelect options={mockJobIndustryOptions} />
+          <BaseSelect options={mockJobLevelOptions} />
+          <BaseSelect options={mockJobArrangementOptions} />
+        </div>
+
+        <SearchBar placeholder="Search job" />
+      </div>
+      <JobListContainer data={mockJobData} />
+    </JobPageLayout>
+  );
 };
 
 export default JobListPage;
