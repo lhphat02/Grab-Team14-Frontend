dd9e3958833997e7f9e22bcff9516c25ae48ae9e
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-08 10:44:14 +0700

    feat(landing): Add industry icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,29 @@
 import logo from './logo.png';
 import logo_svg from './logo_svg.svg';
+import tree from './tree.svg';
+import computer from './computer.svg';
+import accommodation from './accommodation.svg';
+import administration from './administration.svg';
+import construction from './construction.svg';
+import dots from './dots.svg';
+import education from './education.svg';
+import entertainment from './entertainment.svg';
+import healthcare from './healthcare.svg';
+import manufactoring from './manufactoring.svg';
+import money from './money.svg';
 
-export default { logo, logo_svg };
+export default {
+  logo,
+  logo_svg,
+  tree,
+  computer,
+  accommodation,
+  administration,
+  construction,
+  dots,
+  education,
+  entertainment,
+  healthcare,
+  manufactoring,
+  money,
+};
