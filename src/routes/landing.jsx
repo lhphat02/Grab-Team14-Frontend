916a9e3d1a62a8ea6174472015185fcbbce9c964
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-08 11:37:25 +0700

    feat(landing): Add industry selection buttons

--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -1,13 +1,50 @@
+import assets from '../assets';
+import BaseButton from '../components/common/Button/BaseButton';
 import Divider from '../components/common/Divider';
 import LandingSlide from '../components/templates/LandingSlide';
+
+const industries = [
+  { name: 'IT & Media', icon: 'computer' },
+  { name: 'Farming & Agriculture', icon: 'tree' },
+  { name: 'Healthcare', icon: 'healthcare' },
+  { name: 'Accommodation', icon: 'accommodation' },
+  { name: 'Entertainment', icon: 'entertainment' },
+  { name: 'Education', icon: 'education' },
+  { name: 'Construction', icon: 'construction' },
+  { name: 'Manufactoring', icon: 'manufactoring' },
+  { name: 'Administration', icon: 'administration' },
+  { name: 'Finance & Business', icon: 'money' },
+  { name: 'Others', icon: 'dots' },
+];
+
 const LandingPage = () => {
   return (
     <div className="">
-      <LandingSlide>
+      <LandingSlide className={'gap-8'}>
         <h1 className="text-4xl font-black text-center text-prim-1">emploi</h1>
-        <Divider text="Choose your industry" />
+
+        <div className="flex flex-col w-full space-y-4">
+          <Divider text="Choose your industry" />
+
+          <div className="flex flex-wrap justify-center gap-4">
+            {industries.map((industry) => (
+              <BaseButton
+                key={industry.name}
+                className="rounded-full hover:shadow-lg"
+              >
+                <img
+                  src={assets[industry.icon]}
+                  alt="logo"
+                  className="w-6 h-6"
+                />
+                <p>{industry.name}</p>
+              </BaseButton>
+            ))}
+          </div>
+        </div>
       </LandingSlide>
-      <LandingSlide>
+
+      <LandingSlide className={'gap-8'}>
         <div className="items-center justify-center space-y-4 contain-page">
           <h1 className="text-4xl font-black text-center text-prim-1">
             emploi
