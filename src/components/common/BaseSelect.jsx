1f617d22b62e5f322f6f5fe6b83c85ed658915ea
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-10 22:26:51 +0700

    refactor: Update BaseSelect component to include label and disable options

--- src/components/common/BaseSelect.jsx
+++ src/components/common/BaseSelect.jsx
@@ -1,20 +1,32 @@
-const BaseSelect = ({ options, value, onChange }) => {
+const BaseSelect = ({
+  options,
+  value,
+  onChange,
+  defaultValue,
+  disable,
+  label,
+}) => {
   return (
-    <select
-      value={value}
-      onChange={onChange}
-      className="px-4 text-xl bg-white border rounded-lg shadow-sm border-prim-1 focus:outline-none focus:ring-2 focus:ring-prim-2 focus:border-transparent text-prim-1 hover:cursor-pointer"
-    >
-      {options.map((option) => (
-        <option
-          key={option.value}
-          value={option.value}
-          className="text-xl text-prim-1 hover:bg-prim-1 hover:text-white"
-        >
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <div className="flex flex-col w-full">
+      <p className="mb-1 font-semibold text-prim-1">{label}:</p>
+      <select
+        value={value}
+        defaultValue={defaultValue}
+        onChange={onChange}
+        disabled={disable}
+        className={[
+          'w-full px-4 py-2 text-xl border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-prim-2 focus:border-transparent hover:cursor-pointer',
+          disable ? 'bg-gray-100' : 'bg-white',
+        ].join(' ')}
+      >
+        <option value="">Any</option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 };
 
