8551fffe9172b3986873c7f0c2d6215d84d36a50
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-10 22:28:08 +0700

    refactor: Update JobPageLayout to accept a className prop

--- src/components/layouts/JobPageLayout.jsx
+++ src/components/layouts/JobPageLayout.jsx
@@ -1,6 +1,8 @@
-const JobPageLayout = ({ children }) => {
+const JobPageLayout = ({ children, className }) => {
   return (
-    <div className="flex flex-col w-full min-h-screen gap-8 pt-24 bg-gray-100 container-page">
+    <div
+      className={`lex flex-col w-full min-h-screen gap-8 pt-24 bg-gray-100 container-page ${className}`}
+    >
       {children}
     </div>
   );
