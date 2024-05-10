f657acfbc086821c25b5cf211ace3dfac4ff597a
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-10 22:27:21 +0700

    style: Update SearchBar component to use full width and padding for better styling

--- src/components/common/SearchBar.jsx
+++ src/components/common/SearchBar.jsx
@@ -1,10 +1,10 @@
 const SearchBar = ({ placeholder, onChange }) => {
   return (
-    <div className="flex items-center border border-gray-300 rounded-md">
+    <div className="flex items-center w-full border border-gray-300 rounded-md">
       <input
         type="text"
         placeholder={placeholder}
-        className="flex-grow px-4 py-2 rounded-l-md focus:outline-none "
+        className="flex-grow w-full p-2 rounded-l-md focus:outline-none "
         onChange={onChange}
       />
       <button className="px-4 py-2 text-white bg-prim-1 hover:bg-prim-2 rounded-r-md">
