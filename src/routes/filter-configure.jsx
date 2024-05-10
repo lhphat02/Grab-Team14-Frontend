e36859ad627548fbf985a3fd702f027b1438aa00
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-10 22:28:24 +0700

    feat: Add filter configure page with components for configuring filters

--- src/routes/filter-configure.jsx
+++ src/routes/filter-configure.jsx
@@ -1,5 +1,50 @@
+import { useEffect, useState } from 'react';
+
+import FilterPageLayout from '../components/layouts/FiltetPageLayout';
+import FilterConfigureSection from '../components/features/FilterConfigPage/FilterConfigureSection';
+import FilterList from '../components/features/FilterConfigPage/FilterList';
+
+import { mockFilters } from '../constants/mockData';
+
+/**
+ * Represents the page for configuring filters.
+ * @component
+ *
+ * @returns {JSX.Element} The JSX element representing the filter configure page.
+ */
 const FilterConfigurePage = () => {
-  return <div>FilterConfigurePage</div>;
+  const [filterData, setFilterData] = useState(null);
+  const [isEditEnabled, setIsEditEnabled] = useState(false);
+
+  const handleOptionChange = (optionValue) => {
+    setFilterData(
+      mockFilters.find((filter) => filter.value === optionValue) || null
+    );
+  };
+
+  const handleEditToggle = () => {
+    setIsEditEnabled((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const activeFilter =
+      mockFilters.find((filter) => filter.active === true) || null;
+    if (activeFilter) {
+      setFilterData(activeFilter);
+    }
+  }, []);
+
+  return (
+    <FilterPageLayout>
+      <FilterList data={mockFilters} handleOnChange={handleOptionChange} />
+
+      <FilterConfigureSection
+        filterData={filterData}
+        isEditEnabled={isEditEnabled}
+        onEditToggle={handleEditToggle}
+      />
+    </FilterPageLayout>
+  );
 };
 
 export default FilterConfigurePage;
