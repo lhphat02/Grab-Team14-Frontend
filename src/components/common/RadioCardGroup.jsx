bef31e20a0c2d30bece93fd13fbf944b4812f35b
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-16 11:06:51 +0700

    feat: Update RadioCardGroup component to use option IDs instead of values

--- src/components/common/RadioCardGroup.jsx
+++ src/components/common/RadioCardGroup.jsx
@@ -6,13 +6,13 @@ const RadioCardGroup = ({ options, onChange, className }) => {
   useEffect(() => {
     const activeOption = options.find((option) => option.active === true);
     if (activeOption) {
-      setSelectedOption(activeOption.value);
+      setSelectedOption(activeOption.id);
     }
   }, [options]);
 
-  const handleOptionChange = (optionValue) => {
-    setSelectedOption(optionValue);
-    onChange(optionValue);
+  const handleOptionChange = (optionId) => {
+    setSelectedOption(optionId);
+    onChange(optionId);
   };
 
   return (
@@ -21,19 +21,19 @@ const RadioCardGroup = ({ options, onChange, className }) => {
         <label
           key={index}
           className={`border w-full hover:bg-gray-100 rounded-md p-4 cursor-pointer ${
-            selectedOption === option.value
+            selectedOption === option.id
               ? 'bg-gray-100  border-prim-1'
               : 'border-gray-300 bg-white '
           }`}
         >
           <input
             type="radio"
-            value={option.value}
-            checked={selectedOption === option.value}
-            onChange={() => handleOptionChange(option.value)}
+            value={option.id}
+            checked={selectedOption === option.id}
+            onChange={() => handleOptionChange(option.id)}
             className="hidden"
           />
-          <div className="text-lg font-semibold">{option.label}</div>
+          <div className="text-lg font-semibold">{option.name}</div>
           <div className="">
             {option.active ? (
               <p className="text-sm text-prim-1">Active</p>
