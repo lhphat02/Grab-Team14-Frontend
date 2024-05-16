c5f953989596a5ce88b894fb0ceaa5ea856ce598
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-16 11:07:03 +0700

    feat: Update RadioCardGroup component to use option IDs instead of values

--- src/components/features/FilterConfigPage/FilterConfigureSection.jsx
+++ src/components/features/FilterConfigPage/FilterConfigureSection.jsx
@@ -1,41 +1,68 @@
 import {
   BookmarkSquareIcon,
   PencilSquareIcon,
+  FunnelIcon,
 } from '@heroicons/react/24/solid';
 
 import BaseButton from '../../common/Button/BaseButton';
 import BaseSelect from '../../common/BaseSelect';
 import BaseInput from '../../common/BaseInput';
-
-import {
-  mockJobArrangementOptions,
-  mockJobIndustryOptions,
-  mockJobLevelOptions,
-  mockJobTypeOptions,
-} from '../../../constants/mockData';
 import Accordion from '../../common/Accordion';
-import { FunnelIcon } from '@heroicons/react/24/solid';
+import MOCK from '../../../constants/mockData';
+import MultiSelectAccordion from '../../common/MultiSelectAccordion';
+import { useEffect, useState } from 'react';
 
-const materialFilters = [
+const filterAccordions = [
   {
-    label: 'Type',
-    filterOption: mockJobTypeOptions,
-    key: 'type',
+    title: 'Work Type',
+    options: MOCK.OPTIONS.TYPE,
+    defaultSelected: [MOCK.OPTIONS.TYPE[0], MOCK.OPTIONS.TYPE[1]],
   },
   {
-    label: 'Industry',
-    filterOption: mockJobIndustryOptions,
-    key: 'industry',
+    title: 'Industry',
+    options: MOCK.OPTIONS.INDUSTRY,
+    defaultSelected: [MOCK.OPTIONS.INDUSTRY[0], MOCK.OPTIONS.INDUSTRY[1]],
   },
   {
-    label: 'Level',
-    filterOption: mockJobLevelOptions,
-    key: 'level',
+    title: 'Experience Level',
+    options: MOCK.OPTIONS.EXP_LEVEL,
+    defaultSelected: [MOCK.OPTIONS.EXP_LEVEL[0], MOCK.OPTIONS.EXP_LEVEL[1]],
   },
   {
-    label: 'Arrangement',
-    filterOption: mockJobArrangementOptions,
-    key: 'arrangement',
+    title: 'Work Arrangement',
+    options: MOCK.OPTIONS.WORKING_MODE,
+    defaultSelected: [
+      MOCK.OPTIONS.WORKING_MODE[0],
+      MOCK.OPTIONS.WORKING_MODE[1],
+    ],
+  },
+  {
+    title: 'Time',
+    options: MOCK.OPTIONS.TIME,
+    defaultSelected: [MOCK.OPTIONS.TIME[0], MOCK.OPTIONS.TIME[1]],
+  },
+];
+
+const advancedFilters = [
+  {
+    label: 'Exclude titles',
+    placeholder: 'Keywords',
+    key: 'excludeTitles',
+  },
+  {
+    label: 'Exclude companies',
+    placeholder: 'Keywords',
+    key: 'excludeCompanies',
+  },
+  {
+    label: 'Include description',
+    placeholder: 'Keywords',
+    key: 'includeDescription',
+  },
+  {
+    label: 'Exclude description',
+    placeholder: 'Keywords',
+    key: 'excludeDescription',
   },
 ];
 
@@ -53,109 +80,104 @@ const FilterConfigureSection = ({
   filterData,
   isEditEnabled,
   onEditToggle,
-}) => (
-  <section className="flex flex-col items-start justify-start w-full h-full gap-8 p-8 overflow-y-auto bg-white rounded-lg shadow-md">
-    {/* Headbar */}
-    <div className="flex items-center justify-between w-full">
-      <h4 className="text-2xl text-prim-1">{filterData?.label}</h4>
-      <div className="flex gap-2">
-        {filterData?.active ? (
-          <BaseButton outline disabled>
-            <p>Active</p>
-          </BaseButton>
-        ) : (
-          <BaseButton>
-            <p>Activate</p>
-          </BaseButton>
-        )}
+}) => {
+  const [inputValues, setInputValues] = useState({});
 
-        {isEditEnabled ? (
-          <BaseButton onClick={onEditToggle}>
-            <p>Save</p>
-            <BookmarkSquareIcon className="w-6 h-6 " />
-          </BaseButton>
-        ) : (
-          <BaseButton onClick={onEditToggle} className="px-7">
-            <p>Edit</p>
-            <PencilSquareIcon className="w-6 h-6 " />
-          </BaseButton>
-        )}
+  useEffect(() => {
+    setInputValues(filterData);
+  }, [filterData]);
+
+  const handleInputChange = (key, value) => {
+    setInputValues((prevValues) => ({
+      ...prevValues,
+      [key]: value,
+    }));
+  };
+
+  return (
+    <section className="flex flex-col items-start justify-start w-full h-full gap-8 p-8 overflow-y-auto bg-white rounded-lg shadow-md">
+      {/* Headbar */}
+      <div className="flex items-center justify-between w-full">
+        <h4 className="text-2xl text-prim-1">{filterData?.name}</h4>
+
+        <div className="flex gap-2">
+          {filterData?.active ? (
+            <BaseButton outline disabled>
+              <p>Active</p>
+            </BaseButton>
+          ) : (
+            <BaseButton>
+              <p>Activate</p>
+            </BaseButton>
+          )}
+
+          {isEditEnabled ? (
+            <BaseButton onClick={onEditToggle}>
+              <p>Save</p>
+              <BookmarkSquareIcon className="w-6 h-6 " />
+            </BaseButton>
+          ) : (
+            <BaseButton onClick={onEditToggle} className="px-7">
+              <p>Edit</p>
+              <PencilSquareIcon className="w-6 h-6 " />
+            </BaseButton>
+          )}
+        </div>
       </div>
-    </div>
 
-    {/* Basic filters */}
-    <div className="flex justify-between w-full gap-4">
-      {materialFilters.map((filter, index) => (
-        <BaseSelect
-          key={index}
-          options={filter.filterOption}
-          value={filterData?.data?.[filter.key] || ''}
-          label={filter.label}
-          disable={!isEditEnabled}
-        />
-      ))}
-    </div>
-
-    <div className="flex w-full gap-2">
-      {/* Keywords filter */}
-      <div className="flex flex-col w-full gap-4">
-        <div className="flex items-center gap-2">
+      {/* Keywords and location filters */}
+      <div className="flex w-full gap-4">
+        {/* Keywords filter */}
+        <div className="flex flex-col w-full gap-4">
           <p className="font-semibold text-prim-1">Keywords:</p>
-          <p className="text-sm text-gray-500"> (semicolon separated)</p>
-        </div>
-        <BaseInput
-          placeholder="Keywords"
-          disabled={!isEditEnabled}
-          value={filterData?.data?.keyword || ''}
-        />
-      </div>
-      {/* Location filter */}
-      <div className="flex flex-col w-full gap-4">
-        <p className="font-semibold text-prim-1">Location:</p>
-        <BaseInput
-          placeholder="Keywords"
-          disabled={!isEditEnabled}
-          value={filterData?.data?.location || ''}
-        />
-      </div>
-    </div>
 
-    {/* Advanced filters */}
-    <Accordion
-      title={
-        <div className="flex items-center gap-2">
-          <FunnelIcon className="w-6 h-6 text-prim-1" />
-          <p className="font-semibold text-prim-1">Advanced filters</p>
+          <BaseInput
+            placeholder="Keywords"
+            disabled={!isEditEnabled}
+            value={filterData?.keyword || ''}
+          />
         </div>
-      }
-    >
-      {/* Exclude keyword filter */}
-      <div className="flex flex-col w-full gap-4">
-        <div className="flex items-center gap-2">
-          <p className="font-semibold text-prim-1">Exclude keywords:</p>
-          <p className="text-sm text-gray-500"> (semicolon separated)</p>
+        {/* Location filter */}
+        <div className="flex flex-col w-full gap-4">
+          <p className="font-semibold text-prim-1">Location:</p>
+          <BaseInput
+            placeholder="Location"
+            disabled={!isEditEnabled}
+            value={filterData?.location || ''}
+          />
         </div>
-        <BaseInput
-          placeholder="Keywords"
-          disabled={!isEditEnabled}
-          value={filterData?.data?.excludeKeyword || ''}
-        />
       </div>
 
-      {/* Exclude company filter */}
-      <div className="flex flex-col w-full gap-4">
-        <div className="flex items-center gap-2">
-          <p className="font-semibold text-prim-1">Exclude companies:</p>
-          <p className="text-sm text-gray-500"> (semicolon separated)</p>
-        </div>
-        <BaseInput
-          placeholder="Keywords"
-          disabled={!isEditEnabled}
-          value={filterData?.data?.excludeCompany || ''}
-        />
+      {/* Basic filters */}
+      <div className="flex flex-col justify-between w-full gap-4">
+        {filterAccordions.map((accordion, index) => (
+          <MultiSelectAccordion
+            key={index}
+            title={accordion.title}
+            options={accordion.options}
+            defaultSelected={accordion.defaultSelected}
+          />
+        ))}
       </div>
-    </Accordion>
-  </section>
-);
+
+      {/* Advanced filters */}
+      <Accordion
+        title={<p className="text-lg font-semibold text-prim-1">Advanced</p>}
+      >
+        {advancedFilters.map((filter, index) => (
+          <div key={index} className="flex flex-col w-full gap-2">
+            <p className="font-semibold text-prim-1">{filter.label}:</p>
+            <BaseInput
+              placeholder={filter.placeholder}
+              value={inputValues?.[filter.key] || ''}
+              onChange={(e) => handleInputChange(filter.key, e.target.value)}
+              disabled={!isEditEnabled}
+            />
+          </div>
+        ))}
+      </Accordion>
+    </section>
+  );
+};
 
 export default FilterConfigureSection;
