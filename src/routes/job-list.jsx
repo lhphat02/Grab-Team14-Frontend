45707e4f07c63d634d945297177ffdf78a4c6932
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-16 11:10:35 +0700

    refactor: Update JobListPage component to use FilterModalToggle component for filter modal toggle

--- src/routes/job-list.jsx
+++ src/routes/job-list.jsx
@@ -1,55 +1,21 @@
-import { useState } from 'react';
-import BaseSelect from '../components/common/BaseSelect';
-import BaseButton from '../components/common/Button/BaseButton';
 import SearchBar from '../components/common/SearchBar';
 import JobListContainer from '../components/features/JobListPage/JobListContainer';
 import JobPageLayout from '../components/layouts/JobPageLayout';
-import {
-  mockJobData,
-  mockJobArrangementOptions,
-  mockJobIndustryOptions,
-  mockJobLevelOptions,
-  mockJobTypeOptions,
-} from '../constants/mockData';
-import { AdjustmentsHorizontalIcon } from '@heroicons/react/24/solid';
-import BaseModal from '../components/common/BaseModel';
+import MOCK from '../constants/mockData';
+import FilterModalToggle from '../components/features/FilterModal/FilterModalToggle';
 
 const JobListPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-  };
-
   return (
-    <JobPageLayout
-      className={isModalOpen ? 'overflow-y-clip' : 'overflow-y-auto'}
-    >
+    <JobPageLayout>
       <div className="flex flex-col justify-between w-full gap-8 lg:flex-row">
-        <BaseButton onClick={handleOpenModal}>
-          <AdjustmentsHorizontalIcon className="w-6 h-6" />
-          <p className="font-semibold">Filters</p>
-        </BaseButton>
+        <FilterModalToggle />
 
         <div className="w-full lg:max-w-lg">
           <SearchBar placeholder="Search job" />
         </div>
       </div>
 
-      <JobListContainer data={mockJobData} />
-
-      <BaseModal isOpen={isModalOpen} onClose={handleCloseModal}>
-        <div className="flex flex-col justify-between gap-4 p-8">
-          <BaseSelect options={mockJobTypeOptions} label="Type" />
-          <BaseSelect options={mockJobIndustryOptions} label="Industry" />
-          <BaseSelect options={mockJobLevelOptions} label="Level" />
-          <BaseSelect options={mockJobArrangementOptions} label="Arrangement" />
-        </div>
-      </BaseModal>
+      <JobListContainer data={MOCK.JOBS} />
     </JobPageLayout>
   );
 };
