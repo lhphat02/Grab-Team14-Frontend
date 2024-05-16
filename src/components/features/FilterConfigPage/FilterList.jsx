0f40a27baad5655f3e76ec31651340f4d12318db
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-16 11:07:27 +0700

    style: Update FilterList component to use full width for scrollable container

--- src/components/features/FilterConfigPage/FilterList.jsx
+++ src/components/features/FilterConfigPage/FilterList.jsx
@@ -6,7 +6,7 @@ const FilterList = ({ data, handleOnChange }) => {
   return (
     <section className="flex flex-col items-center justify-between w-1/3 h-full gap-4 p-4 bg-white rounded-lg shadow-md">
       <h4 className="text-prim-1">Filters</h4>
-      <div className="h-full pr-2 overflow-y-auto scroll-p-16">
+      <div className="w-full h-full pr-2 overflow-y-auto scroll-p-16">
         <RadioCardGroup options={data} onChange={handleOnChange} />
       </div>
 
