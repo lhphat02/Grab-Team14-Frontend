134b3346e7e3881a821def648caa2aa95f655841
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-19 03:10:10 +0700

    refactor: Update AuthFormLayout to include showLogo prop which handle the logo displaying in the header of the form

--- src/components/layouts/AuthFormLayout.jsx
+++ src/components/layouts/AuthFormLayout.jsx
@@ -1,19 +1,25 @@
-const AuthFormLayout = ({ title, onFormSubmit, formDetails, footer }) => {
+const AuthFormLayout = ({
+  showLogo = true,
+  title,
+  onFormSubmit,
+  formDetails,
+  footer,
+}) => {
   return (
-    <div className="justify-center bg-gray-100 container-page">
-      <div className="flex flex-col w-full max-w-md px-6 py-4 space-y-6 bg-white shadow-md md:px-12 md:py-8 md:space-y-8 rounded-xl">
-        <div className="md:space-y-2">
+    <div className="flex flex-col w-full max-w-md px-6 py-4 space-y-6 bg-white shadow-md md:px-12 md:py-8 md:space-y-8 rounded-xl">
+      <div className="md:space-y-2">
+        {showLogo && (
           <h1 className="font-black text-center text-prim-1">emploi</h1>
-          <h5 className="font-semibold text-center text-prim-1">{title}</h5>
-        </div>
+        )}
+        <h5 className="font-semibold text-center text-prim-1">{title}</h5>
+      </div>
 
-        <form className="flex flex-col space-y-6" onSubmit={onFormSubmit}>
-          {formDetails}
-        </form>
+      <form className="flex flex-col space-y-6" onSubmit={onFormSubmit}>
+        {formDetails}
+      </form>
 
-        <div className="flex flex-col items-center justify-center w-full space-x-2 md:flex-row">
-          {footer}
-        </div>
+      <div className="flex flex-col items-center justify-center w-full space-x-2 md:flex-row">
+        {footer}
       </div>
     </div>
   );
