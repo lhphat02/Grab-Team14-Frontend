e33f357cca4635706771c05d0a9848cad5f14cf1
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-19 03:06:40 +0700

    feat: Add HistoryJobList component for displaying job history in  history page

---
+++ src/components/features/history/HistoryJobList.jsx
@@ -0,0 +1,9 @@
+const HistoryJobList = () => {
+  return (
+    <div className="flex items-center justify-center w-1/2 h-full p-4 overflow-y-auto bg-slate-200">
+      Not implemented
+    </div>
+  );
+};
+
+export default HistoryJobList;
