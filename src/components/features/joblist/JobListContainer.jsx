9255edfd8649ffdb09e605291c9ea34b99541136
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-20 04:33:16 +0700

    refactor: Fetch job data from API in JobListContainer

--- src/components/features/joblist/JobListContainer.jsx
+++ src/components/features/joblist/JobListContainer.jsx
@@ -1,6 +1,12 @@
+import { useEffect } from 'react';
 import JobCard from './card/JobCard';
 
-const JobListContainer = ({ data }) => {
+const JobListContainer = ({ data, queryData }) => {
+  useEffect(() => {
+    // Add here: Fetch job data from API
+    console.log(queryData);
+  }, [queryData]);
+
   return (
     <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
       {data.map((jobData) => (
