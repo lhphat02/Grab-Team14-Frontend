90930b40af7c16d5020a18bdf92f1cbee362cfc9
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-20 02:32:46 +0700

    feat: Add search functionality to job list page

--- src/routes/job-list.jsx
+++ src/routes/job-list.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router-dom';
 import SearchBar from '../components/common/SearchBar';
 import FilterModalToggle from '../components/features/joblist/filter/FilterModalToggle';
 import JobListContainer from '../components/features/joblist/JobListContainer';
@@ -5,6 +6,19 @@ import JobPageLayout from '../components/layouts/JobPageLayout';
 import MOCK from '../constants/mockData';
 
 const JobListPage = () => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  const searchQuery = {
+    keyword: searchParams.get('keyword'),
+    industry: searchParams.get('industry'),
+    workType: searchParams.get('workType'),
+    arrangement: searchParams.get('arrangement'),
+    searchExisting: searchParams.get('searchExisting'),
+  };
+
+  console.log(searchQuery);
+
   return (
     <JobPageLayout>
       <div className="flex flex-col justify-between w-full gap-4 md:gap-8 lg:flex-row">
