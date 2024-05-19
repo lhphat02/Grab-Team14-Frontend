86071498ad5c29decd891a30725354f37087e445
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-20 02:32:56 +0700

    refactor: Update landing page layout and lazy load hero slide component

--- src/routes/landing.jsx
+++ src/routes/landing.jsx
@@ -1,7 +1,11 @@
+import { useRef } from 'react';
 import assets from '../assets';
 import BaseButton from '../components/common/BaseButton';
 import Divider from '../components/common/Divider';
+import HeroSlide from '../components/features/landing/HeroSlide';
 import LandingSlide from '../components/layouts/SlideLayout';
+import { useInView } from 'react-intersection-observer';
+import { ArrowUpIcon } from '@heroicons/react/24/solid';
 
 const industries = [
   { name: 'IT & Media', icon: 'computer' },
@@ -19,45 +23,8 @@ const industries = [
 
 const LandingPage = () => {
   return (
-    <div className="">
-      <LandingSlide className={''}>
-        <h1 className="text-4xl font-black text-center text-prim-1">emploi</h1>
-
-        <div className="flex flex-col w-full space-y-4">
-          <Divider text="Choose your industry" />
-
-          <div className="flex flex-wrap justify-center gap-4">
-            {industries.map((industry) => (
-              <BaseButton
-                key={industry.name}
-                className="py-4 rounded-md hover:shadow-lg"
-              >
-                <img
-                  src={assets[industry.icon]}
-                  alt="logo"
-                  className="w-6 h-6"
-                />
-                <p>{industry.name}</p>
-              </BaseButton>
-            ))}
-          </div>
-        </div>
-      </LandingSlide>
-
-      <LandingSlide className={'gap-8'}>
-        <div className="items-center justify-center space-y-4 contain-page">
-          <h1 className="text-4xl font-black text-center text-prim-1">
-            emploi
-          </h1>
-          <h2 className="text-2xl font-semibold text-center text-prim-1">
-            Simplicity for your career
-          </h2>
-          <h2 className="text-lg font-semibold text-center">
-            Stop Searching, Start Thriving. Find Your Ideal Job Here.
-          </h2>
-        </div>
-      </LandingSlide>
-      <LandingSlide>LandingSlide 3 content</LandingSlide>
+    <div className="w-full">
+      <HeroSlide />
     </div>
   );
 };
