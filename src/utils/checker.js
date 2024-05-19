f4426ffcb3375803913d31a92e440a3fa3a037b1
Lưu Huỳnh Phát <lhphat.dev@gmail.com>
2024-05-20 04:33:55 +0700

    feat: Add isEmptyObject utility function

--- src/utils/checker.js
+++ src/utils/checker.js
@@ -2,3 +2,7 @@ export const isMailValid = (email) => {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailPattern.test(email);
 };
+
+export function isEmptyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
